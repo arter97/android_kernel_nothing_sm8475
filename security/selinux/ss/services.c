5e5599291ed5fa5af32a9ac36770dc7d71f55e3f
kdrag0n <dragon@khronodragon.com>
2018-10-24 19:09:01 -0700

    selinux: don't require auditing
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    
    NVMe platform doesn't have proper SELinux policies and results in more
    audit logs. Remove audit for both configurations for fair comparisons.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3789,6 +3789,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3811,6 +3812,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
