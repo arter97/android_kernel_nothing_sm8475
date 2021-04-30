4690161cbc6d999b5c180da12867ac1be292b408
Nicholas Piggin <npiggin@gmail.com>
2021-04-29 22:58:26 -0700

    mm: HUGE_VMAP arch support cleanup
    This changes the awkward approach where architectures provide init
    functions to determine which levels they can provide large mappings for,
    to one where the arch is queried for each call.
    
    This removes code and indirection, and allows constant-folding of dead
    code for unsupported levels.
    
    This also adds a prot argument to the arch query.  This is unused
    currently but could help with some architectures (e.g., some powerpc
    processors can't map uncacheable memory with large pages).
    
    Link: https://lkml.kernel.org/r/20210317062402.533919-7-npiggin@gmail.com
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Reviewed-by: Ding Tianhong <dingtianhong@huawei.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com> [arm64]
    Cc: Will Deacon <will@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm64/include/asm/vmalloc.h
+++ arch/arm64/include/asm/vmalloc.h
@@ -10,4 +10,10 @@ static inline pgprot_t arch_vmap_pgprot_tagged(pgprot_t prot)
 	return pgprot_tagged(prot);
 }
 
+#ifdef CONFIG_HAVE_ARCH_HUGE_VMAP
+bool arch_vmap_p4d_supported(pgprot_t prot);
+bool arch_vmap_pud_supported(pgprot_t prot);
+bool arch_vmap_pmd_supported(pgprot_t prot);
+#endif
+
 #endif /* _ASM_ARM64_VMALLOC_H */
--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -83,6 +83,12 @@ static inline pgprot_t arch_vmap_pgprot_tagged(pgprot_t prot)
 }
 #endif
 
+#ifndef CONFIG_HAVE_ARCH_HUGE_VMAP
+static inline bool arch_vmap_p4d_supported(pgprot_t prot) { return false; }
+static inline bool arch_vmap_pud_supported(pgprot_t prot) { return false; }
+static inline bool arch_vmap_pmd_supported(pgprot_t prot) { return false; }
+#endif
+
 /*
  *	Highlevel APIs for driver use
  */
