38066d93b8592cf74036519ee77b3a802aeed735
Al Viro <viro@zeniv.linux.org.uk>
2021-04-30 13:40:48 -0400

    iov_iter: clean csum_and_copy_...() primitives up a bit
    1) kmap_atomic() is not needed here, kmap_local_page() is enough.
    2) No need to make sum = csum_block_add(sum, next, off); conditional
    upon next != 0 - adding 0 is a no-op as far as csum_block_add()
    is concerned.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- lib/iov_iter.c
+++ lib/iov_iter.c
@@ -592,9 +592,9 @@ static size_t csum_and_copy_to_pipe_iter(const void *addr, size_t bytes,
 		return 0;
 	do {
 		size_t chunk = min_t(size_t, n, PAGE_SIZE - r);
-		char *p = kmap_atomic(pipe->bufs[i_head & p_mask].page);
+		char *p = kmap_local_page(pipe->bufs[i_head & p_mask].page);
 		sum = csum_and_memcpy(p + r, addr, chunk, sum, off);
-		kunmap_atomic(p);
+		kunmap_local(p);
 		i->head = i_head;
 		i->iov_offset = r + chunk;
 		n -= chunk;
