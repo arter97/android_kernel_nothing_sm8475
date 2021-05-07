ed616670afbf0dfd5f07e84d18e28ba8745e3320
Yury Norov <yury.norov@gmail.com>
2021-05-06 18:03:14 -0700

    lib: add fast path for find_first_*_bit() and find_last_bit()
    Similarly to bitmap functions, users would benefit if we'll handle a case
    of small-size bitmaps that fit into a single word.
    
    While here, move the find_last_bit() declaration to bitops/find.h where
    other find_*_bit() functions sit.
    
    Link: https://lkml.kernel.org/r/20210401003153.97325-11-yury.norov@gmail.com
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Alexey Klimov <aklimov@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Sterba <dsterba@suse.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Jianpeng Ma <jianpeng.ma@intel.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Stefano Brivio <sbrivio@redhat.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: Yoshinori Sato <ysato@users.osdn.me>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/find_bit.c
+++ lib/find_bit.c
@@ -74,7 +74,7 @@ EXPORT_SYMBOL(_find_next_bit);
 /*
  * Find the first set bit in a memory region.
  */
-unsigned long find_first_bit(const unsigned long *addr, unsigned long size)
+unsigned long _find_first_bit(const unsigned long *addr, unsigned long size)
 {
 	unsigned long idx;
 
@@ -85,14 +85,14 @@ unsigned long find_first_bit(const unsigned long *addr, unsigned long size)
 
 	return size;
 }
-EXPORT_SYMBOL(find_first_bit);
+EXPORT_SYMBOL(_find_first_bit);
 #endif
 
 #ifndef find_first_zero_bit
 /*
  * Find the first cleared bit in a memory region.
  */
-unsigned long find_first_zero_bit(const unsigned long *addr, unsigned long size)
+unsigned long _find_first_zero_bit(const unsigned long *addr, unsigned long size)
 {
 	unsigned long idx;
 
@@ -103,11 +103,11 @@ unsigned long find_first_zero_bit(const unsigned long *addr, unsigned long size)
 
 	return size;
 }
-EXPORT_SYMBOL(find_first_zero_bit);
+EXPORT_SYMBOL(_find_first_zero_bit);
 #endif
 
 #ifndef find_last_bit
-unsigned long find_last_bit(const unsigned long *addr, unsigned long size)
+unsigned long _find_last_bit(const unsigned long *addr, unsigned long size)
 {
 	if (size) {
 		unsigned long val = BITMAP_LAST_WORD_MASK(size);
@@ -123,7 +123,7 @@ unsigned long find_last_bit(const unsigned long *addr, unsigned long size)
 	}
 	return size;
 }
-EXPORT_SYMBOL(find_last_bit);
+EXPORT_SYMBOL(_find_last_bit);
 #endif
 
 unsigned long find_next_clump8(unsigned long *clump, const unsigned long *addr,
