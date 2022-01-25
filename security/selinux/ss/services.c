3430261b8929c6d9ea6f7ec957603bf033c32254
Christian Göttsche <cgzones@googlemail.com>
2022-01-25 15:14:15 +0100

    selinux: drop cast to same type
    Both the lvalue scontextp and rvalue scontext are of the type char*.
    Drop the redundant explicit cast not needed since commit 9a59daa03df7
    ("SELinux: fix sleeping allocation in security_context_to_sid"), where
    the type of scontext changed from const char* to char*.
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -1453,7 +1453,7 @@ static int string_to_context_struct(struct policydb *pol,
 	/* Parse the security context. */
 
 	rc = -EINVAL;
-	scontextp = (char *) scontext;
+	scontextp = scontext;
 
 	/* Extract the user. */
 	p = scontextp;
