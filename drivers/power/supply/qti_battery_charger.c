ccbf3c18b71949e7796a8871faebaff5e7a7f837
Juhyung Park <qkrwngud825@gmail.com>
2022-04-20 15:20:40 +0900

    power: supply: introduce qti_power_meter
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1406,6 +1406,8 @@ static const struct power_supply_desc batt_psy_desc = {
 	.property_is_writeable	= battery_psy_prop_is_writeable,
 };
 
+#include "qti_power_meter.c"
+
 static int battery_chg_init_psy(struct battery_chg_dev *bcdev)
 {
 	struct power_supply_config psy_cfg = {};
