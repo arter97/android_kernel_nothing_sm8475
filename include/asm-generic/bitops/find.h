9c8c4b8dfc0c99e049f40e244b0fbe07d90026ec
Valentin Schneider <vschneid@redhat.com>
2022-10-03 16:34:18 +0100

    cpumask: Introduce for_each_cpu_andnot()
    for_each_cpu_and() is very convenient as it saves having to allocate a
    temporary cpumask to store the result of cpumask_and(). The same issue
    applies to cpumask_andnot() which doesn't actually need temporary storage
    for iteration purposes.
    
    Following what has been done for for_each_cpu_and(), introduce
    for_each_cpu_andnot().
    
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>

--- include/asm-generic/bitops/find.h
+++ include/asm-generic/bitops/find.h
@@ -437,6 +437,11 @@ unsigned long find_next_bit_le(const void *addr, unsigned
 	     (bit) < (size);							\
 	     (bit) = find_next_and_bit((addr1), (addr2), (size), (bit) + 1))
 
+#define for_each_andnot_bit(bit, addr1, addr2, size) \
+	for ((bit) = 0;									\
+	     (bit) = find_next_andnot_bit((addr1), (addr2), (size), (bit)), (bit) < (size);\
+	     (bit)++)
+
 /* same as for_each_set_bit() but use bit as value to start with */
 #define for_each_set_bit_from(bit, addr, size) \
 	for ((bit) = find_next_bit((addr), (size), (bit));	\
