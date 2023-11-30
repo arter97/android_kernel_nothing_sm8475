1fe61e7981ba466f33585c2e8ccabc93d1bdb425
Juhyung Park <qkrwngud825@gmail.com>
2023-11-30 22:04:01 +0900

    Revert "ANDROID: random: fix CRC issues with the merge"
    This reverts commit 6d8de02237b3328011e2ba366d4bd1d19652f9b2.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -60,10 +60,6 @@
 #include <asm/irq_regs.h>
 #include <asm/io.h>
 
-// GKI: Keep this header to retain the original CRC that previously used the
-// random.h tracepoints.
-#include <linux/writeback.h>
-
 /*********************************************************************
  *
  * Initialization and readiness waiting.
