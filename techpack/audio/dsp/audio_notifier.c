b2ab2e8cbe5f21c6ed072be9f3b3318598ee1631
Ravulapati Vishnu Vardhan Rao <quic_visr@quicinc.com>
2024-01-23 16:35:55 +0530

    audio_ssr: Mutex dead lock
    notifier_mutex is global mutex and there might be a thread which will
    acquire notifier_mutex which causes other thread to be in wait
    causing deadlock. Using static initialized mutex rectifies issue.
    
    Change-Id: Id8ba30f22419e1a8a7a7356f984db2559197a075
    Signed-off-by: Ravulapati Vishnu Vardhan Rao <quic_visr@quicinc.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- techpack/audio/dsp/audio_notifier.c
+++ techpack/audio/dsp/audio_notifier.c
@@ -248,8 +248,10 @@ static int audio_notifier_dereg_service(int service, int domain)
 		__func__, service_data[service][domain].name,
 		service_data[service][domain].handle);
 
+	mutex_lock(&notifier_mutex);
 	service_data[service][domain].state = AUDIO_NOTIFIER_SERVICE_DOWN;
 	service_data[service][domain].handle = NULL;
+	mutex_unlock(&notifier_mutex);
 done:
 	return ret;
 }
@@ -474,16 +476,16 @@ static int audio_notifier_service_cb(unsigned long opcode,
 		__func__, service_data[service][domain].name, notifier_opcode);
 
 	mutex_lock(&notifier_mutex);
-
 	service_data[service][domain].state = notifier_opcode;
+
 	ret = srcu_notifier_call_chain(&service_data[service][domain].
 		client_nb_list, notifier_opcode, &data);
+	mutex_unlock(&notifier_mutex);
 	if (ret < 0)
 		pr_err("%s: srcu_notifier_call_chain returned %d, service %s, opcode 0x%lx\n",
 			__func__, ret, service_data[service][domain].name,
 			notifier_opcode);
 
-	mutex_unlock(&notifier_mutex);
 done:
 	return NOTIFY_OK;
 }
@@ -521,7 +523,6 @@ int audio_notifier_deregister(char *client_name)
 		ret = -EINVAL;
 		goto done;
 	}
-	mutex_lock(&notifier_mutex);
 	list_for_each_safe(ptr, next, &client_list) {
 		client_data = list_entry(ptr, struct client_data, list);
 		if (!strcmp(client_name, client_data->client_name)) {
@@ -537,7 +538,6 @@ int audio_notifier_deregister(char *client_name)
 			kfree(client_data);
 		}
 	}
-	mutex_unlock(&notifier_mutex);
 done:
 	return ret;
 }
