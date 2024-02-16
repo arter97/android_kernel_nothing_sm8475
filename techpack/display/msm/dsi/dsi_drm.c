5e0916ec34ab2d323a9b075cea0bd13277ef5fde
Juhyung Park <qkrwngud825@gmail.com>
2024-02-16 18:28:15 +0900

    Revert "drm/msm: Eliminate unnecessary snprintf() usage from hot paths"
    This reverts commit 017f8f4edbfde324a62ae87f98047a829fd0203e.
    
    This information is used from the system.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- techpack/display/msm/dsi/dsi_drm.c
+++ techpack/display/msm/dsi/dsi_drm.c
@@ -101,7 +101,16 @@ static void msm_parse_mode_priv_info(const struct msm_display_mode *msm_mode,
 void dsi_convert_to_drm_mode(const struct dsi_display_mode *dsi_mode,
 				struct drm_display_mode *drm_mode)
 {
+	char *panel_caps = "vid";
 	bool fsc_mode = dsi_mode->timing.fsc_mode;
+
+	if ((dsi_mode->panel_mode_caps & DSI_OP_VIDEO_MODE) &&
+		(dsi_mode->panel_mode_caps & DSI_OP_CMD_MODE))
+		panel_caps = "vid_cmd";
+	else if (dsi_mode->panel_mode_caps & DSI_OP_VIDEO_MODE)
+		panel_caps = "vid";
+	else if (dsi_mode->panel_mode_caps & DSI_OP_CMD_MODE)
+		panel_caps = "cmd";
 
 	memset(drm_mode, 0, sizeof(*drm_mode));
 
@@ -131,7 +140,9 @@ void dsi_convert_to_drm_mode(const struct dsi_display_mode *dsi_mode,
 		drm_mode->flags |= DRM_MODE_FLAG_PVSYNC;
 
 	/* set mode name */
-	*drm_mode->name = '\0';
+	snprintf(drm_mode->name, DRM_DISPLAY_MODE_LEN, "%dx%dx%d%s",
+			drm_mode->hdisplay, drm_mode->vdisplay,
+			drm_mode_vrefresh(drm_mode), panel_caps);
 }
 
 static void dsi_convert_to_msm_mode(const struct dsi_display_mode *dsi_mode,
