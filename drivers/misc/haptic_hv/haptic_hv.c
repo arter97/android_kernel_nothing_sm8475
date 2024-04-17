f95040c112f9bd1bc0f5d6705a93daf946059fa8
Juhyung Park <qkrwngud825@gmail.com>
2024-04-17 14:35:11 +0900

    haptic_hv: avoid allocating hap_ctr
    audio_ctrl_list_ins() creates a copy and hap_ctr itself is only used in
    its function context.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/misc/haptic_hv/haptic_hv.c
+++ drivers/misc/haptic_hv/haptic_hv.c
@@ -2793,7 +2793,7 @@ static ssize_t haptic_audio_store(struct device *dev,
 						   vib_dev);
 
 	uint32_t databuf[6] = { 0 };
-	struct aw_haptic_ctr *hap_ctr = NULL;
+	struct aw_haptic_ctr hap_ctr;
 
 	if (!aw_haptic->ram_init) {
 		aw_err("ram init failed, not allow to play!");
@@ -2807,21 +2807,15 @@ static ssize_t haptic_audio_store(struct device *dev,
 				databuf[4], databuf[5]);
 		}
 
-		hap_ctr = (struct aw_haptic_ctr *)kzalloc(
-			sizeof(struct aw_haptic_ctr), GFP_KERNEL);
-		if (hap_ctr == NULL) {
-			aw_err("kzalloc memory fail");
-			return count;
-		}
 		mutex_lock(&aw_haptic->haptic_audio.lock);
-		hap_ctr->cnt = (uint8_t)databuf[0];
-		hap_ctr->cmd = (uint8_t)databuf[1];
-		hap_ctr->play = (uint8_t)databuf[2];
-		hap_ctr->wavseq = (uint8_t)databuf[3];
-		hap_ctr->loop = (uint8_t)databuf[4];
-		hap_ctr->gain = (uint8_t)databuf[5];
-		audio_ctrl_list_ins(aw_haptic, hap_ctr);
-		if (hap_ctr->cmd == 0xff) {
+		hap_ctr.cnt = (uint8_t)databuf[0];
+		hap_ctr.cmd = (uint8_t)databuf[1];
+		hap_ctr.play = (uint8_t)databuf[2];
+		hap_ctr.wavseq = (uint8_t)databuf[3];
+		hap_ctr.loop = (uint8_t)databuf[4];
+		hap_ctr.gain = (uint8_t)databuf[5];
+		audio_ctrl_list_ins(aw_haptic, &hap_ctr);
+		if (hap_ctr.cmd == 0xff) {
 			aw_info("haptic_audio stop");
 			if (hrtimer_active(&aw_haptic->haptic_audio.timer)) {
 				aw_info("cancel haptic_audio_timer");
@@ -2845,7 +2839,6 @@ static ssize_t haptic_audio_store(struct device *dev,
 			}
 		}
 		mutex_unlock(&aw_haptic->haptic_audio.lock);
-		kfree(hap_ctr);
 	}
 	return count;
 }
