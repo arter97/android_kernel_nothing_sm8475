4bc2ea4dee3da91cccaf4c2a023d23609b2d2cf6
Juhyung Park <qkrwngud825@gmail.com>
2024-05-21 15:34:17 +0900

    Revert "BACKPORT: dm verity: set DM_TARGET_IMMUTABLE feature flag"
    This reverts commit c762f435c0e0c62bbfae478a55c89826f6a312cb.
    
    This was already applied.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -1263,7 +1263,6 @@ static struct target_type verity_target = {
 	.name		= "verity",
 	.features	= DM_TARGET_IMMUTABLE,
 	.version	= {1, 7, 0},
-	.features	= DM_TARGET_IMMUTABLE,
 	.module		= THIS_MODULE,
 	.ctr		= verity_ctr,
 	.dtr		= verity_dtr,
