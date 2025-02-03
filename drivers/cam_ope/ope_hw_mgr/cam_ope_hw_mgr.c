234ccd480f2d5a51ee9180d25da16b1e84ea4aa2
Vivek Yadav <quic_viveyada@quicinc.com>
2025-02-03 11:12:21 +0530

    msm: camera: ope: Add IO buffer bound check
    Modify the code to add number of IO buffer bound check
    before accessing while processing IO buffers.
    
    CRs-Fixed: 3991144
    Change-Id: Ifd6817b9a663d483de6b3b485251296fcbc12c6d
    Signed-off-by: Vivek Yadav <quic_viveyada@quicinc.com>
    (cherry picked from commit ed850304196951308430b3efec2f17ced0cdc3b4)

--- drivers/cam_ope/ope_hw_mgr/cam_ope_hw_mgr.c
+++ drivers/cam_ope/ope_hw_mgr/cam_ope_hw_mgr.c
@@ -1958,6 +1958,13 @@ static int cam_ope_mgr_process_cmd_io_buf_req(struct cam_ope_hw_mgr *hw_mgr,
 
 	for (i = 0; i < in_frame_process->batch_size; i++) {
 		in_frame_set = &in_frame_process->frame_set[i];
+
+		if (in_frame_set->num_io_bufs > OPE_MAX_IO_BUFS) {
+			CAM_ERR(CAM_OPE, "Wrong number of io buffers: %u",
+				in_frame_set->num_io_bufs);
+			return -EINVAL;
+		}
+
 		for (j = 0; j < in_frame_set->num_io_bufs; j++) {
 			in_io_buf = &in_frame_set->io_buf[j];
 			for (k = 0; k < in_io_buf->num_planes; k++) {
@@ -1976,13 +1983,14 @@ static int cam_ope_mgr_process_cmd_io_buf_req(struct cam_ope_hw_mgr *hw_mgr,
 
 	for (i = 0; i < ope_request->num_batch; i++) {
 		in_frame_set = &in_frame_process->frame_set[i];
-		ope_request->num_io_bufs[i] = in_frame_set->num_io_bufs;
+
 		if (in_frame_set->num_io_bufs > OPE_MAX_IO_BUFS) {
-			CAM_ERR(CAM_OPE, "Wrong number of io buffers: %d",
+			CAM_ERR(CAM_OPE, "Wrong number of io buffers: %u",
 				in_frame_set->num_io_bufs);
 			return -EINVAL;
 		}
 
+		ope_request->num_io_bufs[i] = in_frame_set->num_io_bufs;
 		for (j = 0; j < in_frame_set->num_io_bufs; j++) {
 			in_io_buf = &in_frame_set->io_buf[j];
 			ope_request->io_buf[i][j] =
