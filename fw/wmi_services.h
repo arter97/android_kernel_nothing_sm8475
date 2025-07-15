808c8d2af8c2a526bc793b9ed552dd1bc5b8b112
spuligil <quic_spuligil@quicinc.com>
2025-07-15 06:01:45 -0700

    fw-api: CL 29754552 - update fw common interface files
    Change-Id: Ic6032c259105e7eae104f27d0468b21152fd5c9b
    CRs-Fixed: 3830439

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -719,6 +719,7 @@ typedef  enum  {
     WMI_SERVICE_11BN = 458, /* Indicates FW supports 802.11bn */
     WMI_SERVICE_HOST_AWARE_POWERSAVE = 459, /* FW supports indicating the powerstate of FW to host */
     WMI_SERVICE_PDEV_DIV_STATES_REPORT = 460, /* FW supports reporting antenna diversity states */
+    WMI_SERVICE_EAPOL_OVER_RAW = 461, /* FW supports sending EAPOL frames in raw mode even when the vdev is brought up in nwifi/ethernet mode */
 
 
     WMI_MAX_EXT2_SERVICE
